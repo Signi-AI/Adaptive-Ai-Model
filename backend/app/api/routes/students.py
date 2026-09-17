680530f729f467ce2f2547c119a1006e9b95ee5d
Abdully <seniorkalima194@gmail.com>
2026-09-17 16:07:38 +0300

    feat: add endpoint to retrieve all students

--- backend/app/api/routes/students.py
+++ backend/app/api/routes/students.py
@@ -4,6 +4,8 @@ api/routes/students.py
 
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
+from app.models.student import Student
+import app.models
 
 from app.core.database import get_db
 from app.schemas.student import StudentCreate, StudentRead
@@ -12,6 +14,13 @@ from app.services import student_service
 router = APIRouter(prefix="/students", tags=["students"])
 
 
+@router.get('/')
+def read_all( db: Session = Depends(get_db)):
+    return db.query(Student).all()
+
+
 @router.post("", response_model=StudentRead, status_code=status.HTTP_201_CREATED)
 def create_student(payload: StudentCreate, db: Session = Depends(get_db)) -> StudentRead:
     return student_service.create_student(db, payload)
+
+
